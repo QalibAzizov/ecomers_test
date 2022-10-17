1a7a2c04e8d91d8613aee8dbca9e2d7b606b90c6
Your Name <azizovgalib@gmail.com>
2022-10-17 19:46:08 +0400

    create models

--- cor/views.py
+++ cor/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from cor.models import *
 
 # Create your views here.
 
 def store(request):
-    context = {}
+    products= Product.objects.all()
+    context = {'products': products}
     return render(request,'store/store.html',context)
 
 def main(request):
@@ -11,9 +13,23 @@ def main(request):
     return render(request,'store/main.html',context)
 
 def card(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete =False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'order': order}
     return render(request,'store/card.html',context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete =False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'order': order}
     return render(request,'store/checkout.html',context)
