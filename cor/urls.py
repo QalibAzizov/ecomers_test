4d1486f764b90161adaba52fc91df4b6f41ccf3a
Your Name <azizovgalib@gmail.com>
2022-10-20 21:30:34 +0400

    cart functionality

--- cor/urls.py
+++ cor/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from cor.views import store, card, checkout
+from cor.views import store, card, checkout, updateItem, processOrder
 
 urlpatterns = [
 
     path('', store , name='store'),
     path('card/', card, name='card'),
     path('checkout/', checkout, name='checkout'),
+    path('update_item/', updateItem, name= 'update_item'),
+    path('proccess_order/', processOrder, name= 'proccess_order'),
+
 ]
